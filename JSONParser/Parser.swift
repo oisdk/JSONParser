6a6b230e57b6cdbc3a8d5745c46cf1a83d4305a2
oisdk <oisinkidney@Oisins-MacBook-Air.local>
2015-09-13 23:07:37 +0100

    cleaned up

--- JSONParser/Parser.swift
+++ JSONParser/Parser.swift
@@ -1,6 +1,15 @@
 public enum JSON {
   case S(String), D(Double), I(Int), B(Bool), A([JSON]), O([String:JSON]), null
 }
+extension JSON {
+  public init(_ s: String)        { self = .S(s) }
+  public init(_ d: Double)        { self = .D(d) }
+  public init(_ i: Int)           { self = .I(i) }
+  public init(_ b: Bool)          { self = .B(b) }
+  public init(_ a: [JSON])        { self = .A(a) }
+  public init(_ o: [String:JSON]) { self = .O(o) }
+  public init()                   { self = .null }
+}
 
 public enum JSONError : ErrorType { case UnBal(String), Parse(String), Empty }
 
@@ -20,12 +29,12 @@ private let wSpace: Set<Character> = [" ", ",", "\n"]
 extension String.CharacterView {
   private var asAt: Result<JSON,JSONError> {
     switch String(trim(wSpace)) {
-    case "null" : return .Some(.null)
-    case "true" : return .Some(.B(true))
-    case "false": return .Some(.B(false))
+    case "null" : return .Some(JSON())
+    case "true" : return .Some(JSON(true))
+    case "false": return .Some(JSON(false))
     case let s: return
-      Int(s).map    { i in .Some(.I(i)) } ??
-      Double(s).map { d in .Some(.D(d)) } ??
+      Int(s)   .map(JSON.init).map(Result.Some) ??
+      Double(s).map(JSON.init).map(Result.Some) ??
       .Error(.Parse(s))
     }
   }
@@ -49,8 +58,8 @@ extension String.CharacterView {
       switch curr.suffixFrom(i).brks("\"", "\"") {
       case let .Error(e) : return .Error(e)
       case let (k,b)?:
-        guard let j = b.indexOf(":") else { return .Error(.Parse(String(b))) }
-        guard let (v,d) = b.suffixFrom(j.successor()).nextDecoded
+        guard case let (v,d)?? =
+          (b.indexOf(":")?.successor()).map(b.suffixFrom)?.nextDecoded
           else { return .Error(.Parse(String(b))) }
         result[String(k)] = v
         curr = d
@@ -63,8 +72,8 @@ extension String.CharacterView {
     guard let i = indexOfNot(wSpace.contains) else { return .Error(.Empty) }
     let v = suffixFrom(i)
     switch self[i] {
-    case "[" : return v.brks("[","]").flatMap { (f,b) in f.asAr.map { a in (.A(a),b) }}
-    case "{" : return v.brks("{","}").flatMap { (f,b) in f.asOb.map { o in (.O(o),b) }}
+    case "[" : return v.brks("[","]").flatMap { (f,b) in f.asAr.map { a in (JSON(a),b) }}
+    case "{" : return v.brks("{","}").flatMap { (f,b) in f.asOb.map { o in (JSON(o),b) }}
     case "\"": return v.brks("\"","\"").map { (f,b) in (.S(String(f)),b) }
     default  : return v.divide(",").map { (f,b) in f.asAt.map { a in (a,b) }} ??
       v.asAt.map { a in (a,"".characters) }
