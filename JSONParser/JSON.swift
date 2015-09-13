4bc1ae343add78cbc745c319fe28891ab6ce6198
oisdk <oisinkidney@Oisins-MacBook-Air.local>
2015-09-13 17:08:43 +0100

    made dict decoding iterative

--- JSONParser/JSON.swift
+++ JSONParser/JSON.swift
@@ -1,12 +1,13 @@
 public enum JSONError : ErrorType { case UnBalancedBrackets, ParseError, Empty }
 
 extension String.CharacterView {
-  func bracketSplit(open: Character, _ close: Character) -> Result<(String.CharacterView, String.CharacterView),JSONError> {
+  func bracketSplit(open: Character, _ close: Character)
+    -> Result<(String.CharacterView, String.CharacterView),JSONError> {
     var count = 1
     for i in indices.dropFirst() {
       if self[i.predecessor()] == "\\" { continue }
       if self[i] == close { --count } else
-        if self[i] == open  { ++count }
+      if self[i] == open  { ++count }
       if count == 0 {
         return .Some(self[startIndex.successor()..<i], suffixFrom(i.successor()))
       }
@@ -15,7 +16,9 @@ extension String.CharacterView {
   }
 }
 
-public enum JSON { case S(String), D(Double), I(Int), B(Bool), A([JSON]), O([String:JSON]), null }
+public enum JSON {
+  case S(String), D(Double), I(Int), B(Bool), A([JSON]), O([String:JSON]), null
+}
 
 extension String {
   private func decodeAsAtom() -> Result<JSON,JSONError> {
@@ -41,27 +44,30 @@ extension String.CharacterView {
         curr = b
         result.append(f)
       case .Error(.Empty): return .Some(result)
-      case let .Error(e): return .Error(e)
+      case let .Error(e) : return .Error(e)
       }
     }
   }
-  
+
   private func decodeAsDict() -> Result<[String:JSON],JSONError> {
-    return decodeAsDictWith([:])
-  }
-  private func decodeAsDictWith(var p: [String:JSON]) -> Result<[String:JSON],JSONError> {
-    guard let i = indexOf("\"") else { return .Some(p) }
-    guard let (k,b) = suffixFrom(i).bracketSplit("\"", "\"") else { return .Error(.UnBalancedBrackets) }
-    guard let j = b.indexOf(":") else { return .Error(.ParseError) }
-    guard let (v,d) = b.suffixFrom(j.successor()).decodeToDelim() else { return .Error(.ParseError) }
-    p[String(k)] = v
-    return d.decodeAsDictWith(p)
+    var (curr,result) = (self,[String:JSON]())
+    while let i = curr.indexOf("\"") {
+      guard let (k,b) = curr.suffixFrom(i).bracketSplit("\"", "\"")
+        else { return .Error(.UnBalancedBrackets) }
+      guard let j = b.indexOf(":") else { return .Error(.ParseError) }
+      guard let (v,d) = b.suffixFrom(j.successor()).decodeToDelim()
+        else { return .Error(.ParseError) }
+      result[String(k)] = v
+      curr = d
+    }
+    return .Some(result)
   }
 }
 
+private let wSpace: Set<Character> = [" ", ",", "\n"]
+
 extension String.CharacterView {
   private func decodeToDelim() -> Result<(JSON, String.CharacterView),JSONError> {
-    let wSpace: Set<Character> = [" ", ",", "\n"]
     guard let i = indexOfNot(wSpace.contains) else { return .Error(.Empty) }
     switch self[i] {
     case "[":
@@ -96,12 +102,12 @@ extension JSON : CustomStringConvertible {
   private func desWithInd(i: String) -> String {
     switch self {
     case let .A(a):
-      let indented = a.map { e in i + "    " + e.desWithInd(i + "    ") }
-      let joined = indented.joinWithSeparator(",\n")
+      let indntd = a.map { e in i + "    " + e.desWithInd(i + "    ") }
+      let joined = indntd.joinWithSeparator(",\n")
       return "[\n" + joined + "\n" + i + "]"
     case let .O(o):
-      let indented = o.map { (k,v) in i + "    \"" + k + "\": " + v.desWithInd(i + "    ")}
-      let joined = indented.joinWithSeparator(",\n")
+      let indntd = o.map { (k,v) in i + "    \"" + k + "\": " + v.desWithInd(i + "    ")}
+      let joined = indntd.joinWithSeparator(",\n")
       return "{\n" + joined + "\n" + i + "}"
     case let .S(s): return "\"" + s + "\""
     case let .B(b): return b.description
