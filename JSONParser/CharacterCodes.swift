4febee7f8c32fb36aa1632e43719a3dcec07b8e1
oisdk <oisin.kidney@gmail.com>
2015-11-23 16:26:30 +0000

    utf8

---
+++ JSONParser/CharacterCodes.swift
@@ -0,0 +1,76 @@
+internal struct Code {
+  
+  static let A: UInt8 = 65
+  static let B: UInt8 = 66
+  static let C: UInt8 = 67
+  static let D: UInt8 = 68
+  static let E: UInt8 = 69
+  static let F: UInt8 = 70
+  static let G: UInt8 = 71
+  static let H: UInt8 = 72
+  static let I: UInt8 = 73
+  static let J: UInt8 = 74
+  static let K: UInt8 = 75
+  static let L: UInt8 = 76
+  static let M: UInt8 = 77
+  static let N: UInt8 = 78
+  static let O: UInt8 = 79
+  static let P: UInt8 = 80
+  static let Q: UInt8 = 81
+  static let R: UInt8 = 82
+  static let S: UInt8 = 83
+  static let T: UInt8 = 84
+  static let U: UInt8 = 85
+  static let V: UInt8 = 86
+  static let W: UInt8 = 87
+  static let X: UInt8 = 88
+  static let Y: UInt8 = 89
+  static let Z: UInt8 = 90
+  
+  static let a: UInt8 = 97
+  static let b: UInt8 = 98
+  static let c: UInt8 = 99
+  static let d: UInt8 = 100
+  static let e: UInt8 = 101
+  static let f: UInt8 = 102
+  static let g: UInt8 = 103
+  static let h: UInt8 = 104
+  static let i: UInt8 = 105
+  static let j: UInt8 = 106
+  static let k: UInt8 = 107
+  static let l: UInt8 = 108
+  static let m: UInt8 = 109
+  static let n: UInt8 = 110
+  static let o: UInt8 = 111
+  static let p: UInt8 = 112
+  static let q: UInt8 = 113
+  static let r: UInt8 = 114
+  static let s: UInt8 = 115
+  static let t: UInt8 = 116
+  static let u: UInt8 = 117
+  static let v: UInt8 = 118
+  static let w: UInt8 = 119
+  static let x: UInt8 = 120
+  static let y: UInt8 = 121
+  static let z: UInt8 = 122
+  
+  static let space : UInt8 = 32
+  static let tab   : UInt8 = 9
+  static let ret   : UInt8 = 13
+  static let newlin: UInt8 = 10
+  
+  static let comma : UInt8 = 44
+  static let colon : UInt8 = 58
+  
+  static let squarC: UInt8 = 93
+  static let squarO: UInt8 = 91
+  
+  static let curliC: UInt8 = 125
+  static let curliO: UInt8 = 123
+  
+  static let quot: UInt8 = 34
+  
+  static let bslash: UInt8 = 92
+  static let fslash: UInt8 = 47
+  
+}
--- JSONParser/FromString.swift
+++ JSONParser/FromString.swift
@@ -1,46 +1,61 @@
-let wspace: Set<Character> = [" ", "\t", "\r", "\n"]
-let wspccl: Set<Character> = [" ", "\t", "\r", "\n", ":"]
-let wspccm: Set<Character> = [" ", "\t", "\r", "\n", ","]
-let wspdlm: Set<Character> = [" ", "\t", "\r", "\n", "," ,"]", "}"]
-let digs:   Set<Character> = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+let wspace: Set<UInt8> = [Code.space, Code.tab, Code.ret, Code.newlin]              // [" ", "\t", "\r", "\n"]
+let wspccl: Set<UInt8> = [Code.space, Code.tab, Code.ret, Code.newlin, Code.colon]          // [" ", "\t", "\r", "\n", ":"]
+let wspccm: Set<UInt8> = [Code.space, Code.tab, Code.ret, Code.newlin, Code.comma]          // [" ", "\t", "\r", "\n", ","]
+let wspdlm: Set<UInt8> = [Code.space, Code.tab, Code.ret, Code.newlin, Code.comma, Code.squarC, Code.curliC] // [" ", "\t", "\r", "\n", "," ,"]", "}"]
+let digs:   Set<UInt8> = [48, 49, 50, 51, 52, 53, 54, 55, 56, 57]
 
-extension CollectionType where Generator.Element == Character, SubSequence.Generator.Element == Character {
+func notWspccm(n: UInt8) -> Bool {
+  switch n {
+  case Code.space, Code.tab, Code.ret, Code.newlin, Code.comma: return false
+  default: return true
+  }
+}
+
+func uChr(n: UInt8) -> Character {
+  return Character(UnicodeScalar(n))
+}
+
+func uStr<S : SequenceType where S.Generator.Element == UInt8>(s: S) -> String {
+  return String(s.lazy.map(uChr))
+}
+
+extension CollectionType where Generator.Element == UInt8, SubSequence.Generator.Element == UInt8 {
   
   public func decodeAtom(var from: Index) -> Result<(JSON,Index),String> {
     switch self[from] {
-    case "n", "N":
-      if (self[++from] == "u" || self[from] == "U") &&
-         (self[++from] == "l" || self[from] == "L") &&
-         (self[++from] == "l" || self[from] == "L") {
+    case Code.n, Code.N: // 'n", "N"
+      if (self[++from] == Code.u || self[from] == Code.U) && // "u", "U"
+         (self[++from] == Code.l || self[from] == Code.L) && // "l", "L"
+         (self[++from] == Code.l || self[from] == Code.L) {  // "l", "L"
           return Result<((JSON,Index)),String>.Some((JSON.null,++from))
       }
-      return .None("Expecting null, found " + String(suffixFrom(from)))
-    case "t", "T":
-      if (self[++from] == "r" || self[from] == "R") &&
-         (self[++from] == "u" || self[from] == "U") &&
-         (self[++from] == "e" || self[from] == "E") {
+      return .None("Expecting null, found " + uStr(suffixFrom(from)))
+    case Code.t, Code.T: // "t", "T"
+      if (self[++from] == Code.r || self[from] == Code.R) && // "r", "R"
+         (self[++from] == Code.u || self[from] == Code.U) && // "u", "U"
+         (self[++from] == Code.e || self[from] == Code.E) {  // "e", "E"
           return Result<((JSON,Index)),String>.Some((JSON.JBool(true),++from))
       }
-      return .None("Expecting true, found " + String(suffixFrom(from)))
-    case "f", "F":
-      if (self[++from] == "a" || self[from] == "A") &&
-         (self[++from] == "l" || self[from] == "L") &&
-         (self[++from] == "s" || self[from] == "S") &&
-         (self[++from] == "e" || self[from] == "E") {
+      return .None("Expecting true, found " + uStr(suffixFrom(from)))
+    case Code.f, Code.F: // f, F
+      if (self[++from] == Code.a || self[from] == Code.A) &&  // "a", "A"
+         (self[++from] == Code.l || self[from] == Code.L) && // "l", "L"
+         (self[++from] == Code.s || self[from] == Code.S) && // "s", "S"
+         (self[++from] == Code.e || self[from] == Code.E) {  // "e", "E"
           return Result<((JSON,Index)),String>.Some((JSON.JBool(false),++from))
       }
-      return .None("Expecting false, found " + String(suffixFrom(from)))
+      return .None("Expecting false, found " + uStr(suffixFrom(from)))
     case let c where digs.contains(c):
       guard let i = indexOf(from.successor(), isElement: wspdlm.contains)
-        else { return .None("Expecting delimiter, found " + String(suffixFrom(from))) }
-      let str = String(self[from..<i])
+        else { return .None("Expecting delimiter, found " + uStr(suffixFrom(from))) }
+      let str = uStr(self[from..<i])
       if let int = Int(str) {
         return Result<((JSON,Index)),String>.Some((JSON.JInt(int),i))
       } else if let flo = Double(str) {
         return Result<((JSON,Index)),String>.Some((JSON.JFloat(flo),i))
       }
-      return .None("Expecting number, found " + String(suffixFrom(from)))
-    default: return .None("Expecting literal, found " + String(suffixFrom(from)))
+      return .None("Expecting number, found " + uStr(suffixFrom(from)))
+    default: return .None("Expecting literal, found " + uStr(suffixFrom(from)))
     }
   }
   
@@ -48,43 +63,43 @@ extension CollectionType where Generator.Element == Character, SubSequence.Gener
     var res = ""
     while true {
       switch self[from++] {
-      case "\"":
+      case Code.quot:
         return Result<((String,Index)),String>
           .Some((res,from))
-      case "\\":
+      case Code.bslash:
         guard let (a,b) = decodeEscaped(from)
-          else { return .None(String(suffixFrom(from))) }
+          else { return .None(uStr(suffixFrom(from))) }
         res.append(a)
         from = b
-      case let c: res.append(c)
+      case let c: res.append(uChr(c))
       }
     }
   }
   
   func decodeEscaped(var from: Index) -> Result<(Character,Index),String> {
     switch self[from++] {
-    case "\"": return Result<((Character,Index)),String>.Some(("\"",from))
-    case "/" : return Result<((Character,Index)),String>.Some(("/",from))
-    case "b" : return Result<((Character,Index)),String>.Some(("\u{8}",from))
-    case "f" : return Result<((Character,Index)),String>.Some(("\u{12}",from))
-    case "n" : return Result<((Character,Index)),String>.Some(("\n",from))
-    case "r" : return Result<((Character,Index)),String>.Some(("\r",from))
-    case "t" : return Result<((Character,Index)),String>.Some(("\t",from))
-    case "u":
+    case Code.quot  : return Result<((Character,Index)),String>.Some(("\"",from))
+    case Code.fslash: return Result<((Character,Index)),String>.Some(("/",from))
+    case Code.b: return Result<((Character,Index)),String>.Some(("\u{8}",from))
+    case Code.f: return Result<((Character,Index)),String>.Some(("\u{12}",from))
+    case Code.n: return Result<((Character,Index)),String>.Some(("\n",from))
+    case Code.r: return Result<((Character,Index)),String>.Some(("\r",from))
+    case Code.t: return Result<((Character,Index)),String>.Some(("\t",from))
+    case Code.u:
       let end = from.advancedBy(4)
-      let str = String(self[from..<end])
+      let str = uStr(self[from..<end])
       guard let usc = UInt32(str, radix: 16)
         else { return .None("Expecting unicode literal, found: " + str) }
       return Result<((Character,Index)),String>
         .Some((Character(UnicodeScalar(usc)),end))
-    default: return .None(String(suffixFrom(from)))
+    default: return .None(uStr(suffixFrom(from)))
     }
   }
   
   func decodeArr(var from: Index) -> Result<([JSON],Index),String> {
     var res: [JSON] = []
-    while let i = indexOf(from, isElement: { c in !wspccm.contains(c) }) {
-      if self[i] == "]" {
+    while let i = indexOf(from, isElement: notWspccm) {
+      if self[i] == Code.squarC {
         return Result<(([JSON],Index)),String>.Some((res,i.successor()))
       }
       switch decode(i) {
@@ -94,23 +109,23 @@ extension CollectionType where Generator.Element == Character, SubSequence.Gener
       case let .None(s): return .None(s)
       }
     }
-    return .None("Array not ended: " + String(suffixFrom(from)))
+    return .None("Array not ended: " + uStr(suffixFrom(from)))
   }
   
   func decodeObj(var from: Index) -> Result<([String:JSON],Index),String> {
     var res: [String:JSON] = [:]
-    while let i = indexOf(from, isElement: { c in !wspccm.contains(c) }) {
-      if self[i] == "}" {
+    while let i = indexOf(from, isElement: notWspccm) {
+      if self[i] == Code.curliC {
         return Result<(([String:JSON],Index)),String>.Some((res,i.successor()))
       }
-      guard self[i] == "\"" else {
-        return .None("Expecting beginning of dictionary key, found: " + String(suffixFrom(from)))
+      guard self[i] == Code.quot else {
+        return .None("Expecting beginning of dictionary key, found: " + uStr(suffixFrom(from)))
       }
       switch decodeString(i.successor()) {
       case let .None(s): return .None(s)
       case let .Some(key,ind):
         guard let valind = indexOf(ind, isElement: { c in !wspccl.contains(c) } )
-          else { return .None("Expecting value in dictionary, found: " + String(suffixFrom(from))) }
+          else { return .None("Expecting value in dictionary, found: " + uStr(suffixFrom(from))) }
         switch decode(valind) {
         case let .None(s): return .None(s)
         case let (val,rind)?:
@@ -125,10 +140,10 @@ extension CollectionType where Generator.Element == Character, SubSequence.Gener
   func decode(from: Index) -> Result<(JSON,Index),String> {
     let i = from.successor()
     switch self[from] {
-    case "[" : return decodeArr(i).map { (a,i) in (JSON.JArray(a),i) }
-    case "{" : return decodeObj(i).map { (o,i) in (JSON.JObject(o),i) }
-    case "\"": return decodeString(i).map { (s,i) in (JSON.JString(s),i) }
-    default  : return decodeAtom(from)
+    case Code.squarO: return decodeArr(i).map { (a,i) in (JSON.JArray(a),i) }
+    case Code.curliO: return decodeObj(i).map { (o,i) in (JSON.JObject(o),i) }
+    case Code.quot  : return decodeString(i).map { (s,i) in (JSON.JString(s),i) }
+    default : return decodeAtom(from)
     }
   }
   
@@ -139,6 +154,6 @@ extension CollectionType where Generator.Element == Character, SubSequence.Gener
 
 extension String {
   public func asJSON() -> Result<JSON,String> {
-    return Array(characters).decode(0).map { (j,_) in j }
+    return Array(utf8).decode(0).map { (j,_) in j }
   }
 }
